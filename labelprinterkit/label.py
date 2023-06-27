ac319919c84df0f7f6effbc78cb06a903aefe503
Benedikt Neuffer <benedikt.neuffer@kit.edu>
2023-06-27 21:06:03 +0200

    Fix import handling if qrcode is not installed

--- labelprinterkit/label.py
+++ labelprinterkit/label.py
@@ -8,7 +8,7 @@ try:
     from qrcode import QRCode as _QRCode
     from qrcode.constants import ERROR_CORRECT_L, ERROR_CORRECT_M, ERROR_CORRECT_H, ERROR_CORRECT_Q
 except ImportError:
-    _QRcode = None
+    _QRCode = None
     ERROR_CORRECT_L = 1
     ERROR_CORRECT_M = 0
     ERROR_CORRECT_Q = 3
