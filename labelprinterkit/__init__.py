a08ffde9a7df9b4c17fa7f793d8b720f2f9fb182
Benedikt Neuffer <benedikt.neuffer@kit.edu>
2023-06-27 17:09:40 +0200

    Fix _all_ variable

--- labelprinterkit/__init__.py
+++ labelprinterkit/__init__.py
@@ -1,4 +1,4 @@
 from pkg_resources import get_distribution
 
-__all__ = ["printers", "backends", "items", "label"]
+__all__ = ["printers", "backends", "label", "job", "page"]
 __version__ = get_distribution(__name__).version
