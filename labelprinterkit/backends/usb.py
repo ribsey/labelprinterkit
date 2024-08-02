c7dbae4600a531c6e8898e6299b94c91b42aef65
Ben Burwood <ben.burwood@streetwave.co>
2024-08-02 20:50:29 +0100

    Add USB Device Kernel Methods

--- labelprinterkit/backends/usb.py
+++ labelprinterkit/backends/usb.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+
 from time import sleep
 
 import usb.core
@@ -8,14 +9,23 @@ from . import BaseBackend
 
 
 class PyUSBBackend(BaseBackend):
-    def __init__(self):
+    def __init__(self) -> None:
+        self._dev = self.get_device()
+
+    def refresh(self) -> None:
+        self._dev = self.get_device()
+
+    @staticmethod
+    def get_device(self) -> usb.core.Device:
+        """Get the PyUSB Device for a USB Printer"""
         dev = usb.core.find(custom_match=self.is_usb_printer)
         if dev is None:
-            raise OSError('Device not found')
-        self._dev = dev
+            raise BrotherPrinterError("No Printer Found")
+        return dev
 
     @staticmethod
     def is_usb_printer(dev) -> bool:
+        """Check if the Device is a USB Printer - by Device Class or Interface Class"""
         if dev.bDeviceClass == 7:
             return True
         for cfg in dev:
@@ -23,7 +33,25 @@ class PyUSBBackend(BaseBackend):
                 return True
         return False
 
-    def write(self, data: bytes):
+    def detach_from_kernel(self) -> None:
+        """
+        Detach the given Device from the Kernel - allowing PyUSB Exclusive Access
+        Required for: usb.core.USBError: [Errno 16] Resource busy
+        """
+        self._dev.reset()
+
+        interface = self._dev[0].interfaces()[0].bInterfaceNumber
+        if self._dev.is_kernel_driver_active(interface):
+            try:
+                self._dev.detach_kernel_driver(interface)
+            except usb.core.USBError:
+                pass
+
+        self._dev.set_configuration()
+
+        self.refresh()
+
+    def write(self, data: bytes) -> None:
         self._dev.write(0x2, data)
 
     def read(self, count: int) -> bytes | None:
