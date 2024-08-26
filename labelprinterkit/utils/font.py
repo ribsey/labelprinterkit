009fd5da132d427ed085d763c7e34383501420e9
Ben Burwood <ben.burwood@streetwave.co>
2024-08-26 18:54:22 +0100

    Update Font Helper Function with Path Parameter

--- labelprinterkit/utils/font.py
+++ labelprinterkit/utils/font.py
@@ -8,11 +8,11 @@ FontPath = NewType("FontPath", Path)
 LINUX_TRUETYPE_FONT_DIRECTORY = "/usr/share/fonts/truetype"
 
 
-def get_linux_fonts() -> dict[str, list[FontPath]]:
+def get_fonts(font_path: str | Path = LINUX_TRUETYPE_FONT_DIRECTORY) -> dict[str, list[FontPath]]:
     """Get all Paths to Linux Fonts : Dict with Key as the Font Name and Value as a List of Paths of Font Variants"""
     fonts = {}
-    for font in os.listdir(LINUX_TRUETYPE_FONT_DIRECTORY):
+    for font in os.listdir(font_path):
         fonts[font] = []
-        for font_variant in os.listdir(f"{LINUX_TRUETYPE_FONT_DIRECTORY}/{font}"):
-            fonts[font].append(FontPath(Path(f"{LINUX_TRUETYPE_FONT_DIRECTORY}/{font}/{font_variant}")))
+        for font_variant in os.listdir(f"{font_path}/{font}"):
+            fonts[font].append(FontPath(Path(f"{font_path}/{font}/{font_variant}")))
     return fonts
